155ac95dcb2ee7a44f2d42892241c64764f9a79d
xdpiqbx <xdpiqbx@gmail.com>
2020-09-25 17:25:53 +0300

    исправляет по ТЗ

--- js/gallery-legacy-code.js
+++ js/gallery-legacy-code.js
@@ -6,37 +6,37 @@ const closeButton = document.querySelector("[data-action=close-lightbox]")
 const lightboxContent = document.querySelector(".lightbox__content")
 const img = lightboxContent.querySelector(".lightbox__image")
 
-const createList = () => {
-    let fullList = ""
-    for(const item of galleryItems){
-        fullList +=`
-        <li class="gallery__item">
-         <a class="gallery__link" href="${item.original}">
-               <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}">
-         </a>
-        </li>
-        `;
-    }
-    galleryList.insertAdjacentHTML('beforeend', fullList)
-}
+for(const item of galleryItems){
+    let li = document.createElement("li")
+    let a = document.createElement("a")
+    let img = document.createElement("img")
 
-createList() // выростил деревцо
-
-// посадил события
-const allA = document.querySelectorAll(".gallery__link")
-for (let link of allA){
-    link.addEventListener("click", (event) => {
+    a.addEventListener("click", (event) => {
         event.preventDefault();
-    })    
+    })
+
+    li.classList = "gallery__item"
+
+    a.classList = "gallery__link"
+    a.setAttribute("href", item.original)
+    
+    img.classList = "gallery__image"
+    img.setAttribute("src", item.preview)
+    img.setAttribute("data-source", item.original)
+    img.setAttribute("alt", item.description)
+
+    a.insertAdjacentElement('beforeend', img)
+    li.insertAdjacentElement('beforeend', a)
+    galleryList.insertAdjacentElement('beforeend', li)
 }
 
 const modalAction = (event) => {
-    document.addEventListener("keydown", closeModalEsc)
-    document.addEventListener("keydown", carousel)
-
     let currentImage = event.target;
     if(currentImage.nodeName === "IMG"){
         lightBox.classList.toggle("is-open")
+    
+        img.setAttribute("src", "")
+        img.setAttribute("alt", "")
     
         img.setAttribute("alt", currentImage.getAttribute("alt"))
         img.setAttribute("src", currentImage.dataset.source)
@@ -45,23 +45,17 @@ const modalAction = (event) => {
 
 const closeModal = () => {
     lightBox.classList.remove("is-open")
-
-    document.removeEventListener("keydown", closeModalEsc)
-    document.removeEventListener("keydown", carousel)
-
-    img.setAttribute("src", "")
-    img.setAttribute("alt", "")
 }
 
 const closeModalEsc = (event) => {
-    if (event.code === "Escape"){
-        closeModal()
+    if (event.code == "Escape"){
+        lightBox.classList.remove("is-open")
     }
 }
 
 const closeModalBox = (event) => {
-    if (event.target.className === "lightbox__content"){
-        closeModal()
+    if (event.target.className == "lightbox__content"){
+        lightBox.classList.remove("is-open")
     }
 }
 
@@ -77,7 +71,7 @@ const carousel = (event) => {
         let currImageFromArray = arrImages.find(image => image.alt == img.alt)
         let currIndex = arrImages.indexOf(currImageFromArray)
         
-        if(event.code === "ArrowRight"){
+        if(event.code == "ArrowRight"){
             if (currIndex == arrImages.length-1){
                 currIndex = 0
                 img.src = arrImages[currIndex].dataset.source
@@ -87,7 +81,7 @@ const carousel = (event) => {
             img.src = arrImages[currIndex + 1].dataset.source
             img.alt = arrImages[currIndex + 1].alt
         }
-        else if(event.code === "ArrowLeft"){
+        else if(event.code == "ArrowLeft"){
             if (currIndex == 0){
                 currIndex = arrImages.length-1
                 img.src = arrImages[currIndex].dataset.source
@@ -103,3 +97,5 @@ const carousel = (event) => {
 galleryList.addEventListener("click", modalAction)
 closeButton.addEventListener("click", closeModal)
 lightBox.addEventListener("click", closeModalBox)
+document.addEventListener("keydown", closeModalEsc)
+document.addEventListener("keydown", carousel)
